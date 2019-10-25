4740657c4f618101d45b2f6ff09f74d9c5a113a5
rebaccazheng <rebaccazheng@users.noreply.github.com>
2019-10-25 09:36:59 +0800

    first add

---
+++ src/page-01/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Contact = (props) => {
+    return (
+        <>
+        <h1>
+        Contact
+        </h1>
+        <h3>會員狀態:{props.userStatus}</h3>
+        </>
+    )
+}
+
+export default Contact
---
+++ src/page-01/News.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const News = (props) => {
+    return (
+        <>
+        <h1>
+        News
+        </h1>
+        <h3>會員狀態:{props.userStatus}</h3>
+        </>
+    )
+}
+
+export default News
